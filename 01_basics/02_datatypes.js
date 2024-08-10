83903ac749c54cc880d5b03c95bb0d553b40c7a9
SiftainEjaz <siftainejaz@gmail.com>
2024-08-10 11:11:13 +0000

    Datatypes Understanding

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" ; //treat all JS Code as newer version. Entire code will be treated as newer versions only.
+//Newer funcctions->classes,modules,etc
+
+//alert("hello")  We are using Nodejs, not browser. Hence will throw error
+
+//Code Readability should be high.
+let name="Siftain"
+let age=18
+let isLoggedIn=true
+
+//PRIMITIVE DATATYPES
+//number -> 2^52
+//bigInt
+//String-> ""
+//boolean -> true/false
+//null -> standalone value (Emtpy)
+//Undefined -> when nothing is defined or assigned to a variable
+//symbol -> unique
+
+
+//Non-primitive Data Types
+//Object
+
+console.log(typeof null)  //Prints object
+console.log(typeof undefined)   //Prints Undefined
+
+
+
+
