130160950e8bc0db07ccc7b9b7b4e8fe8d5f23f7
SiftainEjaz <siftainejaz@gmail.com>
2024-08-14 06:50:48 +0000

    Loops

---
+++ 05_iterations/01_Loops.js
@@ -0,0 +1,29 @@
+ //FOR LOOPS
+// for (let index = 0; index < 10; index++) 
+// {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+
+//While LOOPS
+// let num=10;
+//  while (num<=15) 
+// {
+//     console.log(`Value of num is ${num}`);
+//     num++;
+// }
+
+// console.log(num);
+
+
+//DO WHILE LOOPS
+let score=10;
+do
+{
+    console.log(`value of score is ${score}`);
+    score++;
+}
+while(score<5);
+
