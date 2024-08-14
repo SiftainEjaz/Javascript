fc623b3d7b2636a90b878f3377103d2d5fc35c10
SiftainEjaz <siftainejaz@gmail.com>
2024-08-14 06:16:58 +0000

    controlFlows

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,42 @@
+const balance=1000
+//Highly Unpreferred. UNREADABLE
+// if(balance>500)
+//     console.log("line1"),
+//     console.log("line2");
+
+// const num=1;
+// switch (num) {
+//     case 1:
+//         console.log("1");
+//         console.log("1 is printed");
+        
+//         break;
+//     case 2:
+//         console.log("2");
+//         break;
+        
+//     default:
+//         console.log("3");
+//         break;
+// }
+
+//Nullish Coalescing Operator(??)
+let val1;
+
+//val1=5??10
+//val1=null??10
+val1=undefined??10
+
+console.log(val1);
+
+//Ternary operator
+
+//condition ? true: false
+
+const iceTeaPrice=100
+iceTeaPrice>=80? console.log("more than 80"): console.log("less than 80")
+
+
+
+
+    
